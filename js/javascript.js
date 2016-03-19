4738317769afa7b244d09164c92d169ea99de2c0
hiphoox <norberto.ortigoza@raku.mx>
2016-03-19 15:55:33 -0600

    Initial Commit

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+
+
+/*****
+****** Email subscription / form handling
+*/
+function validateEmailAddress(address){
+  var error = false;
+  var emailFilter = /^.+@.+\..{2,6}$/;
+
+  // check email address
+  if (!(emailFilter.test(address))) { 
+    error = true;
+  }
+
+  // return valid or not valid
+  return !error;
+}
+
